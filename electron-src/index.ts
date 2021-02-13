27ed841c2639f77909510415df9b616d08d916ae
Fábio de Carvalho <callmefabioo@gmail.com>
2021-02-13 14:33:22 -0300

    feat: configure basically everything

--- electron-src/index.ts
+++ electron-src/index.ts
@@ -1,41 +1,37 @@
 // Native
-import { join } from 'path'
-import { format } from 'url'
+import { join } from 'path';
+import { pathToFileURL } from 'url';
 
 // Packages
-import { BrowserWindow, app, ipcMain, IpcMainEvent } from 'electron'
-import isDev from 'electron-is-dev'
-import prepareNext from 'electron-next'
+import { BrowserWindow, app, ipcMain, IpcMainEvent } from 'electron';
+import isDev from 'electron-is-dev';
+import prepareNext from 'electron-next';
 
 // Prepare the renderer once the app is ready
 app.on('ready', async () => {
-  await prepareNext('./renderer')
+  await prepareNext('./renderer');
 
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1440,
+    height: 768,
     webPreferences: {
       nodeIntegration: false,
       preload: join(__dirname, 'preload.js'),
     },
-  })
+  });
 
   const url = isDev
     ? 'http://localhost:8000/'
-    : format({
-        pathname: join(__dirname, '../renderer/out/index.html'),
-        protocol: 'file:',
-        slashes: true,
-      })
+    : pathToFileURL(join(__dirname, '../renderer/out/index.html')).href;
 
-  mainWindow.loadURL(url)
-})
+  mainWindow.loadURL(url);
+});
 
 // Quit the app once all windows are closed
-app.on('window-all-closed', app.quit)
+app.on('window-all-closed', app.quit);
 
 // listen the channel `message` and resend the received message to the renderer process
 ipcMain.on('message', (event: IpcMainEvent, message: any) => {
-  console.log(message)
-  setTimeout(() => event.sender.send('message', 'hi from electron'), 500)
-})
+  console.log(message);
+  setTimeout(() => event.sender.send('message', 'hi from electron'), 500);
+});
